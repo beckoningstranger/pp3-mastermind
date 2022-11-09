9b9986d31eb235fea0a3628a3b369054ccf55a54
Jan Eisen <jan.eisen@tutanota.de>
2022-11-09 08:36:40 +0000

    Validate user input

--- run.py
+++ run.py
@@ -31,11 +31,18 @@ def colored_text(text, color):
 
 
 def take_user_input():
-    u_input = input(f'Please type in a color combination. Possible colors are {colored_text("green", "green")}, {colored_text("yellow", "yellow")}, {colored_text("red", "red")}, {colored_text("pink", "pink")}, {colored_text("cyan", "cyan")} and {colored_text("blue", "blue")}: ')
+    u_input = input(f"Please type in a color combination or 'quit' to exit the game.\nPossible colors are {colored_text("green", "green")}, {colored_text("yellow", "yellow")}, {colored_text("red", "red")}, {colored_text("pink", "pink")}, {colored_text("cyan", "cyan")} and {colored_text("blue", "blue")}:\n")
+    if u_input == 'quit':
+        return u_input
     cleaned_u_input = u_input.split(" ")
-    print(type(cleaned_u_input))
-    print(f'You typed: {cleaned_u_input}')
-    return u_input
+    for item in cleaned_u_input:
+        if item not in available_colors:
+            print(f'Sorry, {item} is not a valid color, please try again.')
+            return "invalid"
+    if len(cleaned_u_input) != 4:
+        print(f'You need to enter exactly 4 colors, but you entered {len(cleaned_u_input)}. Please try again.')
+        return "invalid"
+    return cleaned_u_input
 
 
 while game_is_on:
@@ -43,3 +50,6 @@ while game_is_on:
     user_input = take_user_input()
     if user_input == 'quit':
         game_is_on = False
+    elif user_input != 'invalid':
+        print(f'Received user input {user_input} as valid input.')
+        print('Moving on to the rest of the program')
